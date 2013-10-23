945530691471367b0a4e13063af1be3b88a1d5f4
Matthew Hanson <mhanson@appliedgeosolutions.com>
2013-10-22 20:27:59 -0400

    reorganization gippy data

--- gippy/setup.py
+++ gippy/setup.py
@@ -6,14 +6,20 @@ setup for GIP and gippy
 
 from distutils.core import setup, Extension
 
-gippy_module = Extension(name = '_gippy',
+
+
+gippy_module = Extension(name = 'gippy',
                     sources=['gippylib.i'],
                     swig_opts=['-c++', '-w509','-I../GIP/gip'],
                     include_dirs=['../GIP/gip'],
                     ) 
 
 setup (name = 'gippy',
-        version = '0.1',
+        version = '1.0',
+        description='Python bindings for GIP library',
+        author='Matthew Hanson',
+        author_email='mhanson@appliedgeosolutions.com',
         ext_modules = [gippy_module],
-        py_modules = ["gippy"],
+        py_modules = ['gippy.atmosphere','gippy.GeoVector'],
+        packages = ['gippy.algorithms','gippy.data']
         )
