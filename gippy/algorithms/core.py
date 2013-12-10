15cd0f55fc05779c37d1cbd693dd6448204aec0c
Matthew Hanson <mhanson@appliedgeosolutions.com>
2013-12-10 11:19:52 -0500

    added core algorithm class

---
+++ gippy/algorithms/core.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+class algorithm(object):
+
+	@classmethod
+	def add_options(cls,subparser,parents=[]):
+		pass
+
+	@classmethod
+	def process():
+		pass
+
+	def __init__(self):
+		pass
+
