2a065db7513acd1a4fb4d39f9fa66e4288a86df6
Matthew Hanson <mhanson@appliedgeosolutions.com>
2014-02-27 14:05:35 -0500

    changed rootdir back to production

--- gippy/data/landsat.py
+++ gippy/data/landsat.py
@@ -26,7 +26,7 @@ class LandsatData(Data):
     """ Represents a single date and temporal extent along with (existing) product variations (raw, ref, toaref, ind, ndvi, etc) """
     name = 'Landsat'
     sensors = {'LT4': 'Landsat 4', 'LT5': 'Landsat 5', 'LE7': 'Landsat 7', 'LC8': 'Landsat 8'}
-    _rootdir = '/titan/data/landsat/tiles.dev'
+    _rootdir = '/titan/data/landsat/tiles'
     _tiles_vector = 'landsat_wrs'
     _tiles_attribute = 'pr'
     
