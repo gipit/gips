e04de6f578d61b3d7c8b9e937eb5e393a3015c9c
Matthew Hanson <mhanson@appliedgeosolutions.com>
2014-03-11 09:17:15 -0400

    settings file

---
+++ gippy/settings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+DATABASES = {
+    'tiles': {
+        'NAME': 'geodata',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432',
+    }
+}
