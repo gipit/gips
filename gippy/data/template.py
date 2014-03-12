6573e0ab3064270cd97b4afd9240ae18d7f26ff5
Matthew Hanson <mhanson@appliedgeosolutions.com>
2014-03-12 17:03:46 -0400

    added template for creating adding new data sources

---
+++ gippy/data/template.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+################################################################################
+#    GIPPY: Geospatial Image Processing library for Python
+#
+#    Copyright (C) 2014 Matthew A Hanson
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see <http://www.gnu.org/licenses/>
+################################################################################
+
+from gippy.data.core import Asset, Product, Tile, Data
+
+# Replace DNAME with your dataset name
+
+
+class DNAMEAsset(Asset):
+    _rootpath = '/path/to/data'
+    _sensors = {
+        '': {'description': ''},
+    }
+    # dictionary of assets
+    _assets = {
+        '': {
+            'pattern': '*',
+        }
+    }
+
+
+class DNAMEProduct(Product):
+
+    _products = {}
+    _groups = {}
+
+
+class DNAMETile(Tile):
+    Asset = DNAMEAsset
+
+
+class DNAMEData(Data):
+    Tile = DNAMETile
+    Product = DNAMETile
