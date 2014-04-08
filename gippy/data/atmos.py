4d9677c343f9cef7f1ec002ec44737bede304582
Matthew Hanson <mhanson@appliedgeosolutions.com>
2014-04-08 16:22:49 -0400

    atmos updates

--- gippy/data/atmos.py
+++ gippy/data/atmos.py
@@ -22,7 +22,9 @@ import os
 import datetime
 import gdal
 import ftplib
+import numpy
 
+import gippy
 from gippy.data.inventory import DataInventory
 from gippy.data.core import Repository, Asset, Data
 from gippy.utils import File2List, List2File, VerboseOut
@@ -34,6 +36,7 @@ from pdb import set_trace
 
 class AtmosRepository(Repository):
     _rootpath = '/titan/data/atmos'
+    _cpath = 'composites'
     _datedir = '%Y%j'
 
     @classmethod
@@ -42,6 +45,10 @@ class AtmosRepository(Repository):
         if date != '':
             path = os.path.join(path, str(date.strftime('%Y')), str(date.strftime('%j')))
         return path
+
+    @classmethod
+    def cpath(cls):
+        return os.path.join(cls._rootpath, cls._cpath)
 
     @classmethod
     def find_tiles(cls):
@@ -61,13 +68,12 @@ class AtmosRepository(Repository):
 
 
 class AtmosAsset(Asset):
-    Repository = ModisAtmosRepository
+    Repository = AtmosRepository
 
     # ???? Not specific to MODIS
     _sensors = {
-        'MOD': {'description': 'Terra'},
-        'MYD': {'description': 'Aqua'},
-        'MCD': {'description': 'Combined'}
+        'MOD': {'description': 'MODIS Terra'},
+        'MYD': {'description': 'MODIS Aqua'},
     }
     _assets = {
         'MOD08': {
@@ -82,7 +88,7 @@ class AtmosAsset(Asset):
 
     def __init__(self, filename):
         """ Inspect a single file and get some metadata """
-        super(ModisAtmosAsset, self).__init__(filename)
+        super(AtmosAsset, self).__init__(filename)
 
         bname = os.path.basename(filename)
         self.asset = bname[0:5]
@@ -134,7 +140,7 @@ class AtmosAsset(Asset):
 
 class AtmosData(Data):
     name = 'Globally Gridded Atmospheric Data'
-    Asset = ModisAtmosAsset
+    Asset = AtmosAsset
 
     _products = {
         'aero': {
@@ -142,7 +148,61 @@ class AtmosData(Data):
             # the list of asset types associated with this product
             'assets': ['MOD08'],  # , 'MYD08'],
         },
+        'aerolta': {
+            'description': 'Aerosols, average daily',
+            # the list of asset types associated with this product
+            'assets': ['MOD08'],  # , 'MYD08'],
+        },
     }
+
+    #@classmethod
+    def process_aerolta(cls):
+        """ Calculate long-term averages of aerosol optical thickness """
+        fouts = []
+        for day in range(1, 366):
+            start = datetime.datetime.now()
+            inv = cls.inventory(products=['aero'], days="%s,%s" % (day, day))
+            fnames = [inv[d].tiles[''].products['aero'] for d in inv.dates]
+            if len(fnames) > 0:
+                img = gippy.GeoImage(fnames)
+                fout = os.path.join(cls.Repository.cpath(), 'aerolta', 'aerolta_%s.tif' % day)
+                aerolta = img.Mean(fout)
+                fouts.append(fout)
+                t = datetime.datetime.now()-start
+                VerboseOut('Long-term average aerosol optical depth: processed day %s in %s' % (day, t))
+        start = datetime.datetime.now()
+        aero = gippy.GeoImage(fouts)
+        fout = os.path.join(cls.Repository.cpath(), 'aerolta', 'aerolta.tif')
+        aerolta.Mean(fout)
+        t = datetime.datetime.now()-t
+        VerboseOut('Long-term average aerosol optical depth: processed in %s' % (t))
+        # calculate long term average
+
+    def process(self, products):
+        start = datetime.datetime.now()
+        #bname = os.path.basename(self.assets[''].filename)
+        for product in products:
+            if product == 'aerolta':
+                self.process_aerolta()
+            VerboseOut(' -> %s: processed %s in %s' % (fout, product, datetime.datetime.now()-start))
+
+    def get_point(self, lat, lon, product=''):
+        pixx = int(numpy.round(float(lon) + 179.5))
+        pixy = int(numpy.round(89.5 - float(lat)))
+        roi = gippy.iRect(pixx, pixy, 0, 0)
+        img = self.open(product=product)
+        val = img[0].Read(roi).squeeze()
+        day = self.date.strftime('%j')
+        print 'val', val
+        if val == img[0].NoData():
+            fname = os.path.join(self.Repository.cpath(), 'aerolta', 'aerolta_%s' % day)
+            img = gippy.GeoImage(fname)
+            val = img[0].Read(roi).squeeze()
+            print 'val', val
+        if val == img[0].NoData():
+            print 'still nodata aerosols'
+            val = 0.17
+        return val
 
 
 def main():
