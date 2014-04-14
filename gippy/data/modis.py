9db71c50aa35f659bf5214b9eb0f66498f4908b1
bhbraswell <rbraswell@appliedgeosolutions.com>
2014-04-14 12:54:15 -0400

    update modis, initial code for MOD10 snow and ice

--- gippy/data/modis.py
+++ gippy/data/modis.py
@@ -18,6 +18,7 @@
 #   along with this program. If not, see <http://www.gnu.org/licenses/>
 ################################################################################
 
+import sys
 import os
 import re
 import time
@@ -26,11 +27,23 @@ import urllib
 from osgeo import gdal
 from collections import OrderedDict
 
+import math
+import numpy as np
+
+import gippy
+from gippy import GeoImage
 from gippy.data.core import Repository, Asset, Data
 from gippy.data.inventory import DataInventory
 from gippy.utils import File2List, List2File, VerboseOut
 
 from pdb import set_trace
+
+
+def binmask(arr, bit):
+    """ Return boolean array indicating which elements as binary have a 1 in
+        a specified bit position. Input is Numpy array.
+    """
+    return arr & (1<<(bit-1)) == (1<<(bit-1))
 
 
 class ModisRepository(Repository):
@@ -60,24 +73,36 @@ class ModisAsset(Asset):
         'MYD': {'description': 'Aqua'},
         'MCD': {'description': 'Combined'}
     }
+
     _assets = {
+        'MOD10A1': {
+            'pattern': 'MOD10A1*hdf',
+            'url': 'ftp://n5eil01u.ecs.nsidc.org/SAN/MOST/MOD10A1.005',
+            'startdate': datetime.date(2000, 2, 24),
+            'latency': -3
+        },
         'MOD11A1': {
             'pattern': 'MOD11A1*hdf',
-            'url': 'http://e4ftl01.cr.usgs.gov/MOLT/MOD11A1.005'
+            'url': 'http://e4ftl01.cr.usgs.gov/MOLT/MOD11A1.005',
+            'startdate': datetime.date(2000, 3, 5),
+            'latency': -1
         },
         'MYD11A1': {
             'pattern': 'MYD11A1*hdf',
-            'url': 'http://e4ftl01.cr.usgs.gov/MOLA/MYD11A1.005'
+            'url': 'http://e4ftl01.cr.usgs.gov/MOLA/MYD11A1.005',
+            'startdate': datetime.date(2002, 7, 8),
+            'latency': -1
         }
     }
 
-    # TODO - should be temporal extent
+    # Not used anywhere
     _launchdate = {
         'MOD': datetime.date(1999, 12, 1),
         'MYD': datetime.date(2002, 5, 1),
         'MCD': None
     }
 
+    # Should this be specified on a per asset basis?
     _defaultresolution = [926.625433138333392, -926.625433139166944]
 
     def __init__(self, filename):
@@ -97,7 +122,7 @@ class ModisAsset(Asset):
 
         # I don't understand what this is used for
         # because the asset could be many different things
-        self.products = {'sds1': datafiles[0]}
+        # self.products = {'sds1': datafiles[0]}
 
     def datafiles(self):
         indexfile = self.filename + '.index'
@@ -123,44 +148,37 @@ class ModisAsset(Asset):
         pattern = ''.join(['(', asset, '.A', str(year), str(doy).zfill(3), '.', tile, '.005.\d{13}.hdf)'])
         mainurl = ''.join([httploc, '/', str(year), '.', '%02d' % month, '.', '%02d' % day])
 
-        print "mainurl"
-        print mainurl
-
-        print "pattern"
-        print pattern
+        VerboseOut('%s: mainurl %s, pattern %s' % (asset, mainurl, pattern), 4)
 
         try:
             VerboseOut('opening listing', 1)
             listing = urllib.urlopen(mainurl).readlines()
         except Exception, e:
             listing = None
-            print 'unable to access %s' % mainurl
+            VerboseOut('unable to access %s' % mainurl, 1)
             return 2
 
         cpattern = re.compile(pattern)
         name = None
-
         success = False
 
         for item in listing:
             if cpattern.search(item):
                 if 'xml' in item:
                     continue
-                print 'found in', item.strip()
                 name = cpattern.findall(item)[0]
-                print 'it is', name
+                VerboseOut('found %s in %s' % (name, item.strip()), 4)
                 url = ''.join([mainurl, '/', name])
-                print 'the url is', url
+                VerboseOut('the url is %s' % url, 4)
                 try:
                     urllib.urlretrieve(url, os.path.join(cls.Repository.spath(), name))
-                    print "retrieved %s" % name
+                    VerboseOut('retrieved %s' % name, 4)
                     success = True
                 except Exception, e:
-                    print e
-                    print 'unable to retrieve %s from %s' % (name, url)
+                    VerboseOut('unable to retrieve %s from %s' % (name, url), 4)
 
         if not success:
-            print "did not find a match for %s in listing of %s" % (pattern, mainurl)
+            VerboseOut('did not find a match for %s in listing of %s' % (pattern, mainurl), 4)
             return 1
         else:
             return 0
@@ -172,58 +190,269 @@ class ModisData(Data):
     Asset = ModisAsset
     _pattern = '*.tif'
  
-    # _products = OrderedDict([
-    #     ('temp', {
-    #         'description': 'Surface temperature observations',
-    #         # the list of asset types associated with this product
-    #         'assets': ['MOD11A1', 'MYD11A1'],
-    #     }),
-    #     ('sds1', {
-    #         'description': 'First SDS in the file',
-    #         # the list of asset types associated with this product
-    #         'assets': ['MOD11A1'],
-    #     }),
-    # ])
-
     _products = {
-        'sds1': {
-            'description': 'First SDS in the file',
-            'assets': ['MOD11A1']
-        },
         'temp': {
-            'description': 'Surface temperature observations',
-            'assets': ['MOD11A1', 'MYD11A1']
+            'description': 'Surface temperature data',
+            'assets': ['MYD11A1', 'MOD11A1']
+        },
+        'snow': {
+            'description': 'Snow and ice cover data',
+            'assets': ['MOD10A1']
         }
     }
     
 
+
     def process(self, products):
 
-        print "self._products"
-        print self._products
+        # modis process --temp -t h12v08 -d 2010-001 -v4 --overwrite
 
-        print "products"
-        print products
+        # print "self._products"
+        # print self._products
 
+        # print "products"
+        # print products
+
+        platformnames = {'MOD':'Terra', 'MYD':'Aqua'}
 
         for key, val in products.items():
         
-            print "key, val"
-            print key, val
+            # print "key, val"
+            # print key, val
 
             outfname = os.path.join(self.path, self.basename + '_' + key)        
 
+            print
+            print
             print "outfname"
             print outfname
 
+            if val[0] == 'snow':
+
+                # at some point can also include MYD10A1
+                allsds = []
+                missingassets = []
+                for asset in assets:
+                    try:
+                        sds = self.assets[asset].datafiles()
+                    except:
+                        missingassets.append(asset)
+                    else:
+                        allsds.extend(sds)
+                if len(missingassets) > 0:
+                    print "Skipping missing data:", self.date, self.id, missingassets
+                    continue
+
+
+                for i,sds in enumerate(allsds):
+                    print i,sds
+
+                snowsds = [allsds[0], alldss[3]]
+
+
+
+
+
             if val[0] == 'temp':
+
+                names = {
+                    '0': 'LST_Day_1km',
+                    '1': 'QC_Day',
+                    '2': 'Day_view_time',
+                    '3': 'Day_view_angl',
+                    '4': 'LST_Night_1km',
+                    '5': 'QC_Night',
+                    '6': 'Night_view_time',
+                    '7': 'Night_view_angl',
+                    '8': 'Emis_31',
+                    '9': 'Emis_32',
+                    '10': 'Clear_day_cov',
+                    '11': 'Clear_night_cov',
+                }
 
                 assets = self._products['temp']['assets']
 
-                print "assets"
-                print assets
+                # print "assets"
+                # print assets
+
+                allsds = []
+                missingassets = []
+
+                for asset in assets:
+                    try:
+                        sds = self.assets[asset].datafiles()
+                    except:
+                        missingassets.append(asset)
+                    else:
+                        allsds.extend(sds)
+
+                if len(missingassets) > 0:
+                    print "Skipping missing data:", self.date, self.id, missingassets
+                    continue
+
+                # for i,sds in enumerate(allsds):
+                #     print i,sds
+
+                tempsds = [allsds[0], allsds[4], allsds[12], allsds[16]]                    
+                qcsds = [allsds[1], allsds[5], allsds[13], allsds[17]]
+                hoursds = [allsds[2], allsds[6], allsds[14], allsds[18]]
+
+                tempbands = GeoImage(tempsds) # read only
+                qcbands = GeoImage(qcsds) # read only
+                hourbands = GeoImage(hoursds) # read only
+
+                metanames = {}
+
+                # there are four temperature bands
+                for iband in range(4):
+ 
+                    tempnodatamask = tempbands[iband].NoDataMask()
+                    tempnodatavalue = tempbands[iband].NoDataValue()
+                    hournodatavalue = hourbands[iband].NoDataValue()
+
+                    print "tempnodatavalue", tempnodatavalue
+                    print "hournodatavalue", hournodatavalue
+
+                    temp = tempbands[iband].Read()
+
+                    print "temp.min, temp.max", temp.min(), temp.max()
+
+                    print "len(nodatamask)", (tempnodatamask[tempnodatamask == 1]).size
+                    print "len(temp == 0)", np.sum(temp == 0)
+                    print "len(temp != 0)", np.sum(temp != 0)
+
+                    qc = qcbands[iband].Read()
+
+                    # first two bits are 10 or 11
+                    newmaskbad = binmask(qc, 2)
+
+                    # first two bits are 00 or 01
+                    newmaskgood = ~binmask(qc, 2)
+
+                    # first two bits are 00
+                    newmaskbest = ~binmask(qc, 1) & ~binmask(qc, 2)
+
+                    print "newmaskbest.dtype"
+                    print newmaskbest.dtype
+
+                    if iband == 0:
+                        bestmask = newmaskbest.astype('uint16')
+                    else:
+                        bestmask += (math.pow(2, iband)*newmaskbest).astype('uint16')
+
+                    numbad = np.sum(newmaskbad)
+                    fracbad = np.sum(newmaskbad)/float(newmaskbad.size)
+                    print "numbad, fracbad", numbad, fracbad
+                    print "numgood (by difference)", qc.size - numbad
+
+                    numgood = np.sum(newmaskgood)
+                    fracgood = np.sum(newmaskgood)/float(newmaskgood.size)
+                    print "numgood, fracgood", numgood, fracgood
+
+                    numbest = np.sum(newmaskbest)
+                    fracbest = np.sum(newmaskbest)/float(newmaskbest.size)
+                    print "numbest, fracbest", numbest, fracbest
+
+                    # overpass time
+                    hour = hourbands[iband].Read()
+                    hour = hour[hour != hournodatavalue]
+
+                    try:
+                        hourmin = hour.min()
+                        hourmean = hour.mean()
+                        hourmax = hour.max()
+                        print "hour.min(), hour.mean(), hour.max()", hour.min(), hour.mean(), hour.max()
+                    except:
+                        hourmean = 0
+
+                    basename = tempbands[iband].Basename()
+                    platform = basename[:3]
+                    platform = platformnames[platform]
+
+                    dayornight = basename.split()[1]
+                    dayornight = dayornight.replace('time', '')
+                    assert dayornight in ('day', 'night')
+                    metaname = "MEANOVERPASSTIME_%s_%s" % (platform, dayornight)
+                    metaname = metaname.upper()
+
+                    print "metaname", metaname
+                    metanames[metaname] = str(hourmean)
+
+                    print
 
 
+                print
+                print dir(tempbands)
+                print
+
+                print "tempbands.GetMetaGroup('')"
+                print tempbands.GetMetaGroup('')
+                # print "tempbands.GetMeta()"
+                # print tempbands.GetMeta()
+
+                print "tempbands.Info()"
+                print tempbands.Info()
+                print "tempbands.BandNames()"
+                print tempbands.BandNames()
+                print "tempbands.Basename()"
+                print tempbands.Basename()
+                print "tempbands.DataType()"
+                print tempbands.DataType()
+                print "tempbands.NumBands()"
+                print tempbands.NumBands()
+                print "tempbands.NoDataMask()"
+                print tempbands.NoDataMask()
+
+                print
+                print dir(tempbands[0])
+                print "tempbands[0].Description()"
+                print tempbands[0].Description()
+                print "tempbands[0].Basename()"
+                print tempbands[0].Basename()
+                print "tempbands[0].NoDataValue()"
+                print tempbands[0].NoDataValue()
+                print "tempbands[0].NoData()"
+                print tempbands[0].NoData()
+                print "tempbands[0].Info()"
+                print tempbands[0].Info()
+                print "tempbands[0].Units()"
+                print tempbands[0].Units()
+                print "tempbands[0].Gain()"
+                print tempbands[0].Gain()
+                print "tempbands[0].Offset()"
+                print tempbands[0].Offset()
+                print "tempbands[0].Filename()"
+                print tempbands[0].Filename()
+                print "tempbands[0].Format()"
+                print tempbands[0].Format()
+
+                print "writing", outfname
+                imgout = gippy.GeoImage(outfname, tempbands, gippy.GDT_UInt16, 5)
+                imgout.SetNoData(0)
+                imgout.SetGain(0.02)
+
+                for i in range(4):
+                    imgout[i].Process(tempbands[i])
+
+                    print "imgout[%d].Gain()" % i, imgout[i].Gain()
+                    print "imgout[%d].Offset()" % i, imgout[i].Offset()
+
+                    print imgout[i].Stats()
+
+                print "bestmask.min(), bestmask.max()", bestmask.min(), bestmask.max()
+
+                imgout[4].SetGain(1.0)
+                imgout[4].Write(bestmask)
+
+                print "imgout[4].Gain()", imgout[4].Gain()
+                print "imgout[4].Offset()", imgout[4].Offset()
+
+                print imgout[4].Stats()
+
+                for k, v in metanames.items():
+                    imgout.SetMeta(k, v)
+
+                print imgout.GetMetaGroup('')
 
 
 def main():
