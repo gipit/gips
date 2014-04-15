3ab0293f146094dab708eff846d2ff43ed700a87
Matthew Hanson <mhanson@appliedgeosolutions.com>
2014-04-15 16:22:22 -0400

    atmos additions

--- gippy/data/atmos.py
+++ gippy/data/atmos.py
@@ -143,42 +143,102 @@ class AtmosData(Data):
             VerboseOut(' -> %s: processed %s in %s' % (fout, product, datetime.datetime.now()-start))
 
     @classmethod
-    def process_aerolta_daily(cls, day):
-        """ Calculate long-term averages (lta) for given day of aerosol optical thickness """
+    def process_mean(cls, filenames, fout):
+        """ Calculates mean of all filenames, and per pixel variances """
         start = datetime.datetime.now()
+        if len(filenames) > 0:
+            img = gippy.GeoImage(filenames)
+            imgout = gippy.GeoImage(fout, img, gippy.GDT_Float32, 2)
+            imgout.SetNoData(-32768)
+            img.Mean(imgout[0])
+            meanimg = imgout[0].Read()
+            for band in range(0, img.NumBands()):
+                data = img[band].Read()
+                mask = img[band].DataMask()
+                var = numpy.multiply(numpy.power(data-meanimg, 2), mask)
+                if band == 0:
+                    totalvar = var
+                    counts = mask
+                else:
+                    totalvar = totalvar + var
+                    counts = counts + mask
+            inds = numpy.where(counts == 0)
+            totalvar[inds] = -32768
+            inds = numpy.where(counts != 0)
+            totalvar[inds] = numpy.divide(totalvar[inds], counts[inds])
+            imgout[1].Write(totalvar)
+            t = datetime.datetime.now()-start
+            VerboseOut('%s: mean/variances for %s files processed in %s' % (os.path.basename(fout), len(filenames), t))
+        return imgout
+
+    @classmethod
+    def process_aerolta_daily(cls, day):
+        """ Calculate AOT long-term multi-year averages (lta) for given day """
         inv = cls.inventory(products=['aero'], days="%s,%s" % (day, day))
         fnames = [inv[d].tiles[''].products['aero'] for d in inv.dates]
-        if len(fnames) > 0:
-            img = gippy.GeoImage(fnames)
-            fout = os.path.join(cls.Asset.Repository.cpath('aerolta'), 'aerolta_%s.tif' % str(day).zfill(3))
-            aerolta = img.Mean(fout)
-            t = datetime.datetime.now()-start
-            VerboseOut('Long-term average aerosol optical depth: processed day %s in %s' % (day, t))
+        fout = os.path.join(cls.Asset.Repository.cpath('aerolta'), 'aerolta_%s.tif' % str(day).zfill(3))
+        imgout = cls.process_mean(fnames, fout)
+        return imgout.Filename()
 
     @classmethod
     def process_aerolta(cls):
-        """ Processes daily lta files to generate long-term average file """
-        start = datetime.datetime.now()
-        aero = gippy.GeoImage(fouts)
+        filenames = glob.glob(os.path.join(cls.Asset.Repository.cpath('aerolta'), 'aerolta_*.tif'))
         fout = os.path.join(cls.Asset.Repository.cpath('aerolta'), 'aerolta.tif')
-        aerolta.Mean(fout)
-        t = datetime.datetime.now()-t
-        VerboseOut('Long-term average aerosol optical depth: processed in %s' % (t))
+        cls.process_mean(filenames, fout)
 
     @classmethod
     def process_aerolta_all(cls):
         """ Process all daily long-term average and final lta average file """
-        fouts = []
+        filenames = []
         for day in range(1, 366):
-            fout = cls.process_aerolta_daily(day)
-        cls.process_aerolta()
+            filenames.append(cls.process_aerolta_daily(day))
+        # spatial average
+        #img[band].Smooth(imgout[1])
+        #mean = numpy.multiply(imgout[1].Read(), mask)
 
     def get_point(self, lat, lon, product=''):
         pixx = int(numpy.round(float(lon) + 179.5))
         pixy = int(numpy.round(89.5 - float(lat)))
-        roi = gippy.iRect(pixx, pixy, 0, 0)
+        roi = gippy.iRect(pixx-1, pixy-1, 3, 3)
         img = self.open(product=product)
-        val = img[0].Read(roi).squeeze()
+        nodata = img[0].NoDataValue()
+        vals = img[0].Read(roi).squeeze()
+        # TODO - do this automagically in swig wrapper
+        vals[numpy.where(vals == nodata)] = numpy.nan
+
+        val = vals[1, 1]
+        if val is numpy.nan:
+            val = numpy.nanmean(vals)
+
+        cpath = self.Repository.cpath('aerolta')
+        day = self.date.strftime('%j')
+
+        if val is numpy.nan:
+            img = gippy.GeoImage(os.path.join(cpath, 'aerolta_%s.tif' % str(day).zfill(3)))
+            vals = img[0].Read(roi).squeeze()
+            if vals == numpy.nan:
+                val = numpy.nanmean(vals)
+            else:
+                val = vals[1, 1]
+
+        if val is numpy.nan:
+            img = gippy.GeoImage(os.path.join(cpath, 'aerolta.tif'))
+            vals = img[0].Read(roi).squeeze()
+            if vals == numpy.nan:
+                val = numpy.nanmean(vals)
+            else:
+                val = vals[1, 1]
+
+        set_trace()
+
+        #total = 0
+        #count = 0
+        #for x, y in numpy.ndindex(vals.shape):
+        #    if vals[x,y] != nodata:
+        #        total = total + vals[x,y]
+        #        count = count+1
+        #set_trace()
+
         day = self.date.strftime('%j')
         print 'val', val
         if val == img[0].NoData():
