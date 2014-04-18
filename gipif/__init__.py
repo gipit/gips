bb921caeb66a4dcbbae88036f675d419ab7350ae
Matthew Hanson <mhanson@appliedgeosolutions.com>
2014-04-18 10:10:04 -0400

    more moving around changes

--- gipif/__init__.py
+++ gipif/__init__.py
@@ -18,4 +18,5 @@
 #   along with this program. If not, see <http://www.gnu.org/licenses/>
 ################################################################################
 
-from gippy.GeoVector import GeoVector
+from gipif.GeoVector import GeoVector
+from gipif.core import *
