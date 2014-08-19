675257b9d0cd369750f38e084e445de15aec2127
Matthew Hanson <mhanson@appliedgeosolutions.com>
2014-08-19 16:40:45 -0400

    added truth utility

---
+++ gips/algorithms/tcup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import argparse
+import random
+import numpy
+
+import gippy
+from gips.core import Algorithm
+from gips.utils import VerboseOut
+
+
+class Tcup(Algorithm):
+    name = 'Truth Classification Utility Program'
+    __version__ = '1.0.0'
+
+    def merge(self, files, output, **kwargs):
+        """ Merge multiple truth files into one showing agreement over all """
+        VerboseOut('Merging truth files', 2)
+        random.seed()
+        for i, filename in files:
+            img = gippy.GeoImage(filename)
+            if i == 0:
+                imgarr = img[0].Read()
+                imgout = gippy.GeoImage(output, img)
+                imgout.CopyColorTable(img)
+                imgout.SetNoData(0)
+            else:
+                imgarr2 = img[0].Read()
+                it = numpy.nditer(imgarr, flags=['multi_index'])
+                while not it.finished:
+                    (x, y) = it.multi_index
+                    if imgarr[x, y] != imgarr2[x, y]:
+                        imgarr[x, y] = 0
+                    it.iternext()
+                imgarr2 = None
+            img = None
+        VerboseOut('Created new truth map %s', os.path.basename(output), 2)
+
+    def prune(self, truthfile='', samples=10, **kwargs):
+        """ Prune down a truth image to a max number of samples """
+        suffix = '_pruned%sk' % str(samples)
+        samples = samples * 1000
+        gimg_truth = gippy.GeoImage(truthfile)
+        timg = gimg_truth[0].Read()
+        for i in range(1, numpy.max(timg)+1):
+            locs = numpy.where(timg == i)
+            num = len(locs[0])
+            if num > samples:
+                sample = random.sample(range(0, num-1), num-samples)
+                locs = (locs[0][sample], locs[1][sample])
+                timg[locs] = 0
+            elif num < 10:
+                timg[locs] = 0
+            if num != 0:
+                locs = numpy.where(timg == i)
+                VerboseOut('Class %s: %s -> %s samples' % (i, num, len(locs[0])), 2)
+        gimg_pruned = gippy.GeoImage(os.path.splitext(truthfile)[0] + suffix, gimg_truth)
+        gimg_pruned[0].Write(timg)
+
+    @classmethod
+    def parser(cls):
+        parser0 = argparser.ArgumentParser(add_help=False)
+        subparser = parser0.add_subparsers(dest='command')
+
+        h = 'Merge multiple truth maps into single map of agreement'
+        parser = subparser.add_parser('merge', parents=[cls.vparser()], help=h)
+        parser.add_argument('files', help='List of truth files', nargs='+')
+        parser.add_argument('-o', '--output', help='Output file', required=True)
+
+        parser = subparser.add_parser('prune', parents=[cls.vparser()], help='Prune Truth to S samples per class')
+        parser.add_argument('-s', '--samples', help='Number of samples (in 1K units) to extract', default=10, type=int)
+        parser.add_argument('-t', '--truthfile', help='Truth map to prune')
+
+
+def main():
+    Tcup.main()
