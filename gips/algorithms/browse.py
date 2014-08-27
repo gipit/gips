dda9d5fa661557c0bc22695ff4391799037d9e2c
Matthew Hanson <mhanson@appliedgeosolutions.com>
2014-08-27 13:41:04 -0400

    initial working version of gips_browse

--- gips/algorithms/browse.py
+++ gips/algorithms/browse.py
@@ -2,6 +2,7 @@
 
 import argparse
 
+import gippy
 from gips.algorithms.core import Algorithm
 
 
@@ -10,14 +11,19 @@ class Browse(Algorithm):
     name = 'GIPS Browse'
     __version__ = '1.0.0'
 
-    def run(self, **kwargs):
+    def run(self, quality=75, **kwargs):
         for date in self.inv.dates:
-            for p in self.inv.requested_products:
-                
+            for p in self.inv.product_list(date):
+                try:
+                    filename = self.inv[date]
+                    gippy.BrowseImage(self.inv[date].open(p), quality)
+                except:
+                    pass
 
     @classmethod
     def parser(cls, parser0):
         cls.add_project_parser(parser0)
+        parser0.add_argument('-q', '--quality', help='JPG Quality', default=75)
         return parser0
 
 
