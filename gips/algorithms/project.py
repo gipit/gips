a41f82f68e442ddd561940171fb5c71d8036247e
Matthew Hanson <mhanson@appliedgeosolutions.com>
2014-08-27 17:11:10 -0400

    moved gips_browse function into gips_project

--- gips/algorithms/project.py
+++ gips/algorithms/project.py
@@ -2,19 +2,23 @@
 
 import argparse
 
+import gippy
 from gips.algorithms.core import Algorithm
+from pdb import set_trace
 
 
 class Project(Algorithm):
     name = 'GIPS Project Utilities'
     __version__ = '1.0.0'
 
-    def browse(self, **kwargs):
-        """ Create browse imagery for project """
+    def browse(self, quality=75, **kwargs):
         for date in self.inv.dates:
-            for p in self.inv.requested_products:
-                fname = self.inv[date][p]
-                print fname
+            for p in self.inv.product_list(date):
+                try:
+                    filename = self.inv[date]
+                    gippy.BrowseImage(self.inv[date].open(p), quality)
+                except:
+                    pass
 
     @classmethod
     def parser(cls, parser0):
@@ -22,6 +26,7 @@ class Project(Algorithm):
         kwargs = cls.parser_kwargs(project=True)
 
         parser = subparser.add_parser('browse', help='Create browse imagery', **kwargs)
+        parser0.add_argument('-q', '--quality', help='JPG Quality', default=75)
 
         return parser0
 
