6f84cebbe6a1f792c0e53aff067f7500153cbed5
Matthew Hanson <mhanson@appliedgeosolutions.com>
2014-10-21 09:30:52 -0400

    mapreduce moved into gips.utils

--- gips/utils.py
+++ gips/utils.py
@@ -23,6 +23,8 @@ import errno
 import gippy
 import datetime
 import calendar
+import multiprocessing
+import numpy
 
 
 class Colors():
@@ -72,7 +74,7 @@ def File2List(filename):
 
 def List2File(lst, filename):
     f = open(filename, 'w')
-    f.write('\n'.join(lst)+'\n')
+    f.write('\n'.join(lst) + '\n')
     f.close()
 
 
@@ -80,7 +82,7 @@ def RemoveFiles(filenames, extensions=['']):
     for f in filenames:
         for ext in ([''] + extensions):
             try:
-                os.remove(f+ext)
+                os.remove(f + ext)
             except OSError as e:
                 if e.errno != errno.ENOENT:
                     raise
@@ -131,7 +133,7 @@ def _parse_date(dstring, last=False):
     """ Parses string of YYYY or YYYY-MM or YYYY-MM-DD or YYYY-DOY and returns date object """
     d = dstring.split('-')
     if len(d) == 2 and len(d[1]) == 3:
-        dttmp = datetime.datetime(int(d[0]), 1, 1) + datetime.timedelta(days=int(d[1])-1)
+        dttmp = datetime.datetime(int(d[0]), 1, 1) + datetime.timedelta(days=int(d[1]) - 1)
         d[1] = dttmp.month
         d.append(dttmp.day)
     if (not last):
@@ -154,3 +156,72 @@ def parse_dates(dstring):
         return (_parse_date(d1), _parse_date(d2, True))
     except:
         return (_parse_date(dstring), _parse_date(dstring, True))
+
+
+def chunk_data(datasz, nchunks=100):
+    """ Create chunks given input data size """
+    if len(datasz) == 3:
+        chaxis = 1
+    else:
+        chaxis = 0
+    chunksz = int(datasz[chaxis] / nchunks)
+    remainder = datasz[chaxis] - chunksz * nchunks
+    chszs = [chunksz] * (nchunks - remainder) + [chunksz + 1] * remainder
+    chunks = []
+    for ichunk in range(nchunks):
+        # This is being inverted because gippy is X x Y, whereas numpy is Y x X
+        #chunks.append(gippy.Recti(0, sum(chszs[:ichunk]), datasz[2], chszs[ichunk]))
+        chunks.append([0, sum(chszs[:ichunk]), datasz[2], chszs[ichunk]])
+    return chunks
+
+
+def _mr_init(_readfunc, _func):
+    """ Put functions witin global namespace for each process """
+    global readfunc, func
+    readfunc = _readfunc
+    func = _func
+
+
+#def _worker_2d_to_1d(chunk):
+#    """ Apply a function to a piece of the 2-D datain array and return 1-D output """
+#    dat = data[chunk[0]:chunk[1], :]
+#    valid = numpy.all(~numpy.isnan(dat), axis=1)
+#    chunk = numpy.zeros(dat.shape[0])
+#    chunk[valid] = func(dat[valid, :])
+#    return chunk
+
+
+def _mr_worker_3d_to_2d(chunk):
+    """ Reduces multiple band image (bands x rows x cols) to single band image (rows x cols) """
+    data = readfunc(gippy.Recti(chunk[0], chunk[1], chunk[2], chunk[3]))
+    valid = numpy.all(~numpy.isnan(data), axis=0)
+    output = numpy.zeros((data.shape[1], data.shape[2]))
+    output[valid] = func(data[:, valid])
+    return output
+
+
+def map_reduce(datasz, readfunc, func, nchunks=100, nproc=2):
+    """ Chunk up data read from readfunc, apply func, then reassemble into output array """
+    chunks = chunk_data(datasz, nchunks=nchunks)
+    pool = multiprocessing.Pool(nproc, initializer=_mr_init, initargs=(readfunc, func))
+    data_parts = pool.map(_mr_worker_3d_to_2d, chunks)
+    # reassemble data
+    dataout = numpy.zeros((datasz[1], datasz[2]))
+    for i, ch in enumerate(chunks):
+        dataout[ch.y0():ch.y1(), ch.x0():ch.x1()] = data_parts[i]
+    return dataout
+
+
+# old code utilizing shared memory array
+# Chunk it up
+#chunksz = int(data.shape[0] / nproc)
+#extra = data.shape[0] - chunksz * nproc
+#chunks = [chunksz] * (nproc - extra) + [chunksz + 1] * extra
+
+#queue = multiprocessing.Queue()
+#from agspy.contrib import shmarray
+#classmap = shmarray.create_copy(classmap)
+#tmp = numpy.ctypeslib.as_ctypes(classmap)
+#cmap = sharedctypes.Array(tmp._type_, tmp, lock=False)
+
+
