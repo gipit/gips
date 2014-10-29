73feef970a4202b9ddd6ca5cbb673a5eaa4a56d0
Matthew Hanson <mhanson@appliedgeosolutions.com>
2014-10-29 15:01:08 -0400

    updated changelist and bumped beta version

--- gips/version.py
+++ gips/version.py
@@ -17,4 +17,4 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program. If not, see <http://www.gnu.org/licenses/>
 ################################################################################
-__version__ = '0.7.1b1'
+__version__ = '0.7.1b2'
