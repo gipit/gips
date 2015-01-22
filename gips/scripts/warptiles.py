b84b97152fd0aea84799a4b876c9e39854a7b99c
Matthew Hanson <mhanson@appliedgeosolutions.com>
2015-01-22 13:58:02 -0500

    final versions gips_copytiles and gips_warptiles

--- gips/scripts/warptiles.py
+++ gips/scripts/warptiles.py
@@ -21,8 +21,6 @@
 #   along with this program. If not, see <http://www.gnu.org/licenses/>
 ################################################################################
 
-import shutil
-
 from gips import __version__ as gipsversion
 from gips.parsers import GIPSParser
 from gips.data.core import data_class
@@ -30,13 +28,14 @@ from gips.utils import Colors, VerboseOut, mkdir
 
 
 def main():
-    title = Colors.BOLD + 'GIPS Copy Tiles Utility v%s' % gipsversion + Colors.OFF
+    title = Colors.BOLD + 'GIPS Warp Tiles Utility v%s' % gipsversion + Colors.OFF
 
     # argument parsing
     parser0 = GIPSParser(description=title)
     parser0.add_inventory_parser()
     parser0.add_process_parser()
     parser0.add_project_parser()
+    parser0.add_warp_parser()
     parser0.add_data_sources()
     args = parser0.parse_args()
 
@@ -48,19 +47,27 @@ def main():
         # create top level directory
         suffix = '' if args.suffix is None else '_' + args.suffix
         if args.datadir is None:
-            args.datadir = args.command + '_tiles' + suffix
+            args.datadir = args.command + '_tiles'
+            if args.res is None:
+                args.res = cls.Asset._defaultresolution
+            if args.res[0] == args.res[1]:
+                resstr = str(args.res[0])
+            else:
+                resstr = '%sx%s' % (args.res[0], args.res[1])
+            args.datadir = '%s_%s%s' % (args.datadir, resstr, suffix)
         mkdir(args.datadir)
 
-        # copy the tiles
+        # warp the tiles
         for date in inv.dates:
             for tid in inv[date].tiles:
                 inv[date].tiles[tid].process(args.products, overwrite=False)
-                inv[date].tiles[tid].copy(args.datadir, args.products, overwrite=args.overwrite)
+                inv[date].tiles[tid].copy(args.datadir, args.products, args.crop,
+                                          inv.spatial.site, args.res, args.interpolation, args.overwrite)
 
     except Exception, e:
         import traceback
         VerboseOut(traceback.format_exc(), 4)
-        print 'Copy Tiles error: %s' % e
+        print 'Warp Tiles error: %s' % e
 
 
 if __name__ == "__main__":
