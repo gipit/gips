d3cbdf94f4bd583ae069f664e7047972f23368a6
Matthew Hanson <mhanson@appliedgeosolutions.com>
2015-02-02 14:10:58 -0500

    oops, fixed numprocs option

--- gips/parsers.py
+++ gips/parsers.py
@@ -81,7 +81,7 @@ class GIPSParser(argparse.ArgumentParser):
         group = parser.add_argument_group('processing options')
         group.add_argument('--overwrite', help='Overwrite existing output file(s)', default=False, action='store_true')
         group.add_argument('--chunksize', help='Chunk size in MB', default=512.0)
-        group.add_argument('--numproc', help='Desired number of processors (if allowed)', default=2, type=int)
+        group.add_argument('--numprocs', help='Desired number of processors (if allowed)', default=2, type=int)
         group.add_argument('--format', help='Format for output file', default="GTiff")
         self.parent_parsers.append(parser)
         return parser
